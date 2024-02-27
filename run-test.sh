8763e7d8a469da33fa2d60fd81e7a5bdca0c7d4b
Brandon Ching <bching9@gmail.com>
2024-02-26 21:39:25 -0700

    add folder to testing other teams

--- run-test.sh
+++ run-test.sh
@@ -16,10 +16,10 @@
 trap 'echo "Ctrl+C pressed. Exiting..."; exit 1' INT
 
 # Input Folder Name 
-programs=("python3 main4.py") # TODO: Add the programs to run here (e.g. "python3 main.py", "java Main", "g++ main.cpp -o main", etc.)
-input_folder="test/input"
+programs=("python3 main4.py" "python3 main3.py" "python3 main2.py" ) # TODO: Add the programs to run here (e.g. "python3 main.py", "java Main", "g++ main.cpp -o main", etc.)
+input_folder="test/algobowl-in"
 output_folder="test-out"
-valid_output_folder="test/output"
+valid_output_folder="test/algobowl-out"
 timeout_duration=300
 
 ############################################
